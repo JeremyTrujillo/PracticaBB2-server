a633186b32f1827a6a90473dce5fc26a7607114d
Jeremy Trujillo <trujillosanchezjeremy@gmail.com>
2023-05-17 23:09:08 +0100

    Initial commit.

---
+++ src/main/java/com/example/practicabitboxer2/PracticaBitBoxer2Application.java
@@ -0,0 +1,13 @@
+package com.example.practicabitboxer2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaBitBoxer2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticaBitBoxer2Application.class, args);
+    }
+
+}
